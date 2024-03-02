b79a338fc7b0d40b76af485091b0f48a287123a3
Matias <mun19005@byui.edu>
2024-03-02 01:33:33 -0300

    mongo conection

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const mongodb = require('./data/database');
 const app = express();
 
-const port = 3000;
+
+
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+if(err){
+  console.log(err);
+}
+else{
+  app.listen(port, () => {console.log(`Web Server is listening at port ${port}`)});
+}
+});
+
+
+
+
+
+
  
-app.listen(process.env.PORT || port);
-  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+
+
+
+
