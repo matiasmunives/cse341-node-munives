82a298eb7d4d7c368a57b274c39ecbd5034c904c
Matias <mun19005@byui.edu>
2024-03-03 02:07:50 -0300

    commit new

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 
 const routes = require ('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
+
  
 router.get('/', (req, res) => {res.send('Hello World');});
+
+router.use('/users', require('./users'));
 
 module.exports = routes;
